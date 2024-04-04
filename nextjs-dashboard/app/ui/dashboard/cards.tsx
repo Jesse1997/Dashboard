f6fd6dd4a17ee2a1934028bd896569f26746d514
Jesse Van Loock <jessevanloock@gmail.com>
2024-04-04 16:29:19 +0200

    final changes

--- nextjs-dashboard/app/ui/dashboard/cards.tsx
+++ nextjs-dashboard/app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +15,19 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
@@ -56,3 +58,7 @@ export function Card({
     </div>
   );
 }
+function fetchInvoiceData(): { totalPaidInvoices: any; totalPendingInvoices: any; numberOfInvoices: any; numberOfCustomers: any; } | PromiseLike<{ totalPaidInvoices: any; totalPendingInvoices: any; numberOfInvoices: any; numberOfCustomers: any; }> {
+  throw new Error('Function not implemented.');
+}
+
